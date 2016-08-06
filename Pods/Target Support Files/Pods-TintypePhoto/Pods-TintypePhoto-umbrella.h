1de55f596defa1901b00588164eeb7869ad4c122
adrian <adrmarszalek@gmail.com>
2016-08-06 18:12:49 +0200

    added cocoapods, changed file structure

---
+++ Pods/Target Support Files/Pods-TintypePhoto/Pods-TintypePhoto-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_TintypePhotoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TintypePhotoVersionString[];
+
---
+++ Pods/Target Support Files/Pods-TintypePhotoTests/Pods-TintypePhotoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TintypePhotoTests : NSObject
+@end
+@implementation PodsDummy_Pods_TintypePhotoTests
+@end
